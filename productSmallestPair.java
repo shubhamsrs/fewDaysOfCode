34486d61340fc3668766473eca7ba6d3af57a45c
Shubham <53858416+shubhamsrs@users.noreply.github.com>
2021-09-15 22:38:21 +0530

    Create productSmallestPair.java

---
+++ productSmallestPair.java
@@ -0,0 +1,54 @@
+/*
+The function def ProductSmallestPair(sum, arr) accepts an integers sum and an integer array arr of size n. Implement the function to find the pair, (arr[j], arr[k]) where j!=k, Such that arr[j] and arr[k] are the least two elements of array (arr[j] + arr[k] <= sum) and return the product of element of this pair
+
+NOTE
+
+Return -1 if array is empty or if n<2
+Return 0, if no such pairs found
+All computed values lie within integer range
+Example
+
+Input
+
+sum:9
+
+Arr:5 2 4 3 9 7 1
+
+Output: 2
+
+Explanation:-> Pair of least two element is (2, 1) 2 + 1 = 3 < 9, Product of (2, 1) 2*1 = 2. Thus, output is 2
+*/
+
+import java.util.*;
+public class Main
+{
+	public static int productSmallestPair(int sum, int arr[]){
+	    int temp;
+	    for(int i=0;i<arr.length;i++){
+	        for(int j=i+1;j<arr.length;j++){
+	            if(arr[i]>arr[j]){
+	                temp=arr[i];
+	                arr[i]=arr[j];
+	                arr[j]=temp;
+	            }
+	        }
+	    }
+	    if(arr[0]+arr[1]<=sum)
+	        return arr[0]*arr[1];
+	   else
+	        return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+        int sum = sc.nextInt ();
+        int n = sc.nextInt ();
+        int arr[] = new int[n];
+    
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt ();
+        }
+        System.out.println(productSmallestPair (sum,arr));
+
+	}
+}
